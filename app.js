340185d85336e189b131f3d6bd8c299819a16866
Mrudula Wakodkar <121959719+Mrudula274@users.noreply.github.com>
2023-11-25 10:40:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -28,9 +28,9 @@ https.get(url,(response)=>{
        const description = weatherdata.weather[0].description
        //console.log(description);
        res.write("<h1>The tempreature in "+query+" is " + temp + "degree celcius</h1>")
-       //res.write("<p>The weather description is "+description+"</p>")
-        //res.write("<h3>CI/CD : Continuous Integration and Continuouse Deployment</h3>")
-        
+       res.write("<p>The weather description is "+description+"</p>")
+        res.write("<h3>CI/CD : Continuous Integration and Continuouse Deployment</h3>")
+        res.write("Hello")
     })
 })
 // res.send("This is for checking our server")
